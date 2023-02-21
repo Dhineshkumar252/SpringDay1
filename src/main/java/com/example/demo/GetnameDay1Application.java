71091a47f05ed8dc2df250b1e7e33b87462b3711
Dhineshumar252 <727721euit031@skcet.ac.in>
2023-02-21 12:10:20 +0530

    first commit

---
+++ src/main/java/com/example/demo/GetnameDay1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetnameDay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetnameDay1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/name.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class name {
+	String platform="Iam neo";
+	@RequestMapping("/hi")
+	public String getname() {
+		return "Welcome to " + platform;
+	}
+
+}
